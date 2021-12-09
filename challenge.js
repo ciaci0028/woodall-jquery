6c67da818bb46984ea0456fd383ecda2e1a60b84
Sabrina Ciaciura <ciaci0028@gmail.com>
2021-12-09 11:49:51 -0600

    made kitty rotate

--- challenge.js
+++ challenge.js
@@ -1,5 +1,13 @@
 $(document).ready(onReady);
 
 function onReady () {
-    $('#kitty').on('click', )
+    $('#kitty').on('click', spinKitty )
+
+    let kitty = $('#kitty');
+    function spinKitty() {
+        $(kitty).css(
+            {'transform': 'rotate(' + 90 + 'deg)'}
+        );
+        $(kitty).append('<p>Kitty why you so silly?<p>');
+    }
 }
